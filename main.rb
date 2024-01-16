404f3d6c8c2e2d397ae94bd18c2a9992a3c040dc
MatheoGaillac <glc.matheo@gmail.com>
2024-01-16 08:41:51 +0100

    Feat : Fetch administration per departments

---
+++ main.rb
@@ -0,0 +1,16 @@
+require 'json'
+file_path = "assets/api-lannuaire-administration.json"
+
+json_data = File.read(file_path)
+data = JSON.parse(json_data)
+departments = ['16', '24', '33', '31', '32', '64', '65', '79', '86']
+
+data.each do |administration|
+  if administration['adresse']
+    address_array = JSON.parse(administration['adresse'])
+    code_postal = address_array.find do |code|
+      departments.any? { |department| code['code_postal'].start_with?(department) }
+    end
+    puts code_postal
+  end
+end
